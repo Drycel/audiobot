c20db1ad37676f393bef3c2c2867b6fec5170747
Drycel <valeraispanec11@gmail.com>
2024-06-12 21:58:01 +0200

    Replace old code with new code

--- bot.py
+++ bot.py
@@ -3,6 +3,10 @@ from pytube import YouTube
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from dotenv import load_dotenv
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Завантаження змінних середовища з файлу .env
 load_dotenv()
@@ -12,16 +16,18 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 # Переконайтеся, що токен завантажено
 if not TELEGRAM_TOKEN:
-    print("Помилка: Токен не знайдено у файлі .env")
+    logger.error("Помилка: Токен не знайдено у файлі .env")
     exit(1)
 else:
-    print(f"Токен успішно завантажено: {TELEGRAM_TOKEN}")
+    logger.info(f"Токен успішно завантажено: {TELEGRAM_TOKEN}")
 
 def start(update: Update, context: CallbackContext) -> None:
+    logger.info("Виклик функції start")
     update.message.reply_text('Привіт! Надішліть мені посилання на YouTube відео, і я надішлю вам аудіо!')
 
 def download_audio(update: Update, context: CallbackContext) -> None:
     url = update.message.text
+    logger.info(f"Отримано URL: {url}")
     update.message.reply_text('Завантаження аудіо, будь ласка, зачекайте...')
 
     try:
@@ -34,9 +40,11 @@ def download_audio(update: Update, context: CallbackContext) -> None:
         context.bot.send_audio(chat_id=update.message.chat_id, audio=open(audio_file, 'rb'))
         os.remove(audio_file)
     except Exception as e:
+        logger.error(f'Сталася помилка: {e}')
         update.message.reply_text(f'Сталася помилка: {e}')
 
 def main() -> None:
+    logger.info("Запуск бота")
     updater = Updater(TELEGRAM_TOKEN, use_context=True)
 
     dispatcher = updater.dispatcher
@@ -48,3 +56,4 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+
